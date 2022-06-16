4f3fee2e3bc1edc83052f8ec5f65a417fb4eddc5
Suraj Yadav <ysuraj877@gmail.com>
2022-06-16 23:59:04 +0530

    added styling with tachyons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,25 @@ import { useState } from "react";
 import { useForm } from "react-hook-form"
 
 function App() {
-  const { register, handleSubmit } = useForm();
-  const [parsed, setParsed] = useState("")
-  const onSubmit = data => axios({method: 'POST', url: "http://localhost:5000/", data})
-    .then(x => setParsed(x.data))
-    .catch(e => console.error(e.message))
-  return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <textarea {...register("grammar")}></textarea>
-        <input type="text" {...register("input")} />
-        <button type="submit">submit</button>
-        <pre>{decodeURIComponent(parsed)}</pre>
-      </form>
-    </div>
-  );
+    const { register, handleSubmit } = useForm();
+    const [parsed, setParsed] = useState("")
+    const onSubmit = data => axios({method: 'POST', url: "http://localhost:5000/", data})
+	  .then(x => setParsed(x.data))
+	  .catch(e => console.error(e.message))
+    return (
+	<div className="flex w-100">
+	  <form onSubmit={handleSubmit(onSubmit)} className="flex flex-column w-100">
+	    <div className="w-100 flex flex-column pa2">
+	      <textarea {...register("grammar")} className="h5 consolas"></textarea>
+	      <input type="text" {...register("input")} className="mt2 h2"/>
+	    </div>
+	    <button type="submit" className="bg-blue flex ma2 bn white pa2 br1 pointer hover-bg-dark-blue">submit</button>
+	  </form>
+	  <div className="w-100 flex pa2">
+	    <pre>{decodeURIComponent(parsed)}</pre>
+	  </div>
+	</div>
+    );
 }
 
 export default App;
